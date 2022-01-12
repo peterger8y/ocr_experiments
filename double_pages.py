5d011c72a6458665696c8af8f30a6a35b9c15d9c
peterger8y <peterger8y@gmail.com>
2022-01-12 10:38:58 -0600

    input gc_loader for model training

--- double_pages.py
+++ double_pages.py
@@ -5,17 +5,17 @@ import random
 
 
 def concatenator_p2():
+    """ for novel data generation """
     dir = 'iam_data/pargs/'
     tar_dir = 'iam_data/bigger_text_set/'
     i = 0
-    j = 0
     alpha = 0
     for path in os.listdir(dir):
         if path[-3:] == 'png':
             im_curr = cv2.imread(dir + path, 0)
             if i == 0:
-                boundary1 = random.randint(0, 256)
-                boundary2 = random.randint(boundary1, 255)
+                boundary1 = random.randint(200, 255)
+                boundary2 = random.randint(boundary1, 256)
                 if boundary1 == boundary2:
                     boundary1 -= 1
                 padd = np.random.randint(boundary1, boundary2, (im_curr.shape), np.uint8)
@@ -41,15 +41,16 @@ def concatenator_p2():
             elif i == 3:
                 im_prev = cv2.resize(im_prev, (im_curr.shape[1], im_curr.shape[0]), interpolation=cv2.INTER_AREA)
                 im_second_prev = cv2.resize(im_second_prev, (im_curr.shape[1], im_curr.shape[0]), interpolation=cv2.INTER_AREA)
-                print(im_prev.shape, im_second_prev.shape, im_curr.shape)
+
                 im_newer = np.concatenate([im_prev, im_second_prev])
-                im_curr = np.concatenate([im_curr, im_newer])
+                im_curr = np.concatenate([im_newer, im_curr])
                 cv2.imwrite(tar_dir + str(alpha) + '.png', im_curr)
                 with open(dir + path[:-3] + 'txt', 'r') as filed:
                     text = filed.read()
                     string_along += ' ' + text
                 with open(tar_dir + str(alpha) + '.txt', 'x') as filed:
                     filed.write(string_along)
+
                 alpha += 1
             elif i == 4:
                 cv2.imwrite(tar_dir + str(alpha) + '.png', im_curr)
@@ -57,10 +58,11 @@ def concatenator_p2():
                     text = filed.read()
                 with open(tar_dir + str(alpha) + '.txt', 'x') as filed:
                     filed.write(text)
+                alpha+=1
             i += 1
             i = i % 5
-            cv2.imshow('image', im_curr)
-            cv2.waitKey(0)
+            # cv2.imshow('image', im_curr)
+            # cv2.waitKey(0)
 
 
 def concatenator():
@@ -85,6 +87,21 @@ def concatenator():
             cv2.imshow('image', np.uint8(im))
             cv2.waitKey(0)
 
+def train_gc_loader():
+    dir = 'iam_data/bigger_text_set/'
+    train_gc =  'iam_data/alt_train.gc'
+    val_gc = 'iam_data/alt_val.gc'
+    i = 0
+    for path in os.listdir(dir):
+        if path[-3:] == 'png':
+            if i % 10 == 0:
+                with open('iam_data/alt_val.gc', 'a') as fi:
+                    fi.write(path + '\n')
+            else:
+                with open('iam_data/alt_train.gc', 'a') as fi:
+                    fi.write(path + '\n')
+
+
 
 if __name__ == '__main__':
-    concatenator_p2()
+    train_gc_loader()
